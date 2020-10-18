1cb485548b7249174078ad78d64245adaa4e267f
amarnath.mishra <amarnath.mishra@paytmmoney.com>
2020-10-18 10:46:07 +0530

    base code

---
+++ src/main/java/com/amarnathtravels/routeplanner/RouteplannerApplication.java
@@ -0,0 +1,13 @@
+package com.amarnathtravels.routeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteplannerApplication.class, args);
+	}
+
+}
